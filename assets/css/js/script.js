8a5a79a1afbc64fd50266a03be4057f55ee42fa3
Laidybird <predaveronica72@yahoo.com>
2022-11-02 09:22:12 +0000

    Fixing the file path

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,21 +1,19 @@
 // Wait for the DOM to finnish loading before running the game
 //Get the button elements and event listeners to them
-
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons){
+    for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit"){
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
-            }else {
+            } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked${gameType}`);
+                alert(`You clicked ${gameType}`);
             }
-        })
+        });
     }
-})
-
+});
 
 function runGame(){
 
